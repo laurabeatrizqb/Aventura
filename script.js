2891ae6194470072bf5af18dd49254409c3938a3
laurabeatrizqb <laura.beatriz.fernandes@escola.pr.gov.br>
2025-09-11 09:36:48 -0300

    ok

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 const avanca =document. querySelectorAll('.btn-proximo');
+const reiniciar = document.getElementById('btn-reiniciar');
 
-avanca.for
+avanca.forEach(button={
+button.addEventListener('click', function(){
+const atual = document.querySelector('.ativo');
+const proximoPasso= 'passo-'+this.getElementById('data-proximo');
+
+atual.classList.remove('ativo');
+const proximoElemento= document.getElementById(proximoPasso);
+
+if(proximoElemento){
+   proximoElemento.classList.add('ativo');
+   }else{
+         console.error(`Elemento com ID "${proximoPasso}" nao encontrado.`)
+        }
+        });
+      });
